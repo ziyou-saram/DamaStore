ca7290a863acc50af804b914c08fa5b02f1273ee
ziyou-saram <zhumabayevalikhan@gmail.com>
2022-05-18 11:45:56 +0600

    DamaStore

--- server.js
+++ server.js
@@ -7,6 +7,59 @@ const port = process.env.PORT || 8080
 const http = require("http")
 const request = require("request")
 
+const extendTimeoutMiddleware = (req, res, next) => {
+    const space = ' ';
+    let isFinished = false;
+    let isDataSent = false;
+
+    // Only extend the timeout for API requests
+    if (!req.url.includes('/api')) {
+        next();
+        return;
+    }
+
+    res.once('finish', () => {
+        isFinished = true;
+    });
+
+    res.once('end', () => {
+        isFinished = true;
+    });
+
+    res.once('close', () => {
+        isFinished = true;
+    });
+
+    res.on('data', (data) => {
+        // Look for something other than our blank space to indicate that real
+        // data is now being sent back to the client.
+        if (data !== space) {
+            isDataSent = true;
+        }
+    });
+
+    const waitAndSend = () => {
+        setTimeout(() => {
+            // If the response hasn't finished and hasn't sent any data back....
+            if (!isFinished && !isDataSent) {
+                // Need to write the status code/headers if they haven't been sent yet.
+                if (!res.headersSent) {
+                    res.writeHead(202);
+                }
+
+                res.write(space);
+
+                // Wait another 15 seconds
+                waitAndSend();
+            }
+        }, 15000);
+    };
+
+    waitAndSend();
+    next();
+};
+
+app.use(extendTimeoutMiddleware);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
